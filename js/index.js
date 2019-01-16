604437622264ae47150262660ce795f47cb85676
Brent <w0414104@nscc.ca>
2019-01-16 11:48:07 -0400

    All devicons added

--- js/index.js
+++ js/index.js
@@ -10,14 +10,15 @@ $("li").click(function(e) {
   $(this).addClass('active');
 });
 
+
+
 window.addEventListener("scroll", () => {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    document.querySelector(".nav__logo").style.fontSize = "0.7em";
     document.getElementsByClassName("nav")[0].style.padding = "0em 4em 0em 4em";
     document.getElementsByClassName("nav")[0].style.backgroundColor = "hsl(0, 100%, 60%)";
   } else {
-    document.getElementsByClassName("nav__logo")[0].style.fontSize = "1em";
     document.getElementsByClassName("nav")[0].style.padding = "1em 4em 1em 4em";
     document.getElementsByClassName("nav")[0].style.backgroundColor = "#2b2b2b";
   }
 });
+
